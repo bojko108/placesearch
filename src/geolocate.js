33ae37e9756439fb767a466a1488f3a15564af0f
bojko108 <bojko108@gmail.com>
2022-07-25 13:35:08 +0300

    migrate to manifest v3

--- src/geolocate.js
+++ src/geolocate.js
@@ -1,42 +1,47 @@
-let geolocate = (place) => {
-    chrome.tabs.create({
-        url: `https://www.google.com/maps/search/${place}`
-    });
+const geolocate = (place) => {
+  chrome.tabs.create({
+    url: `https://www.google.com/maps/search/${place}`,
+  });
 };
 
-// geolocate from title bar button:
-chrome.browserAction.onClicked.addListener(() => {
-    chrome.tabs.query({
-        currentWindow: true,
-        active: true
-    }, (tab) => {
-        let query = prompt('Enter a place name:');
-        if (query) {
-            geolocate(query);
-        }
-    });
-});
-
 // geolocate from page context menu:
-chrome.contextMenus.create({
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.contextMenus.create({
+    id: 'my_geolocate',
     title: 'Geolocate: %s',
     contexts: ['selection'],
-    onclick: (info) => {
-        geolocate(info.selectionText);
+  });
+});
+chrome.contextMenus.onClicked.addListener((e) => {
+  geolocate(e.selectionText);
+});
+
+// geolocate from title bar button:
+chrome.action.onClicked.addListener(() => {
+  chrome.tabs.query(
+    {
+      currentWindow: true,
+      active: true,
+    },
+    (tab) => {
+      let query = prompt('Enter place name:');
+      if (query) {
+        geolocate(query);
+      }
     }
+  );
 });
 
 //geolocate from the address bar: keyword - gm
-chrome.omnibox.onInputEntered.addListener(
-    (text) => {
-        let qString = `https://www.google.com/maps/search/${text}`;
-        chrome.tabs.query({
-            'currentWindow': true,
-            'active': true
-        }, (tab) => {
-            chrome.tabs.update(
-                tab[0].id, { 'url': qString }
-            );
-        });
+chrome.omnibox.onInputEntered.addListener((text) => {
+  let qString = `https://www.google.com/maps/search/${text}`;
+  chrome.tabs.query(
+    {
+      currentWindow: true,
+      active: true,
+    },
+    (tab) => {
+      chrome.tabs.update(tab[0].id, { url: qString });
     }
-);
+  );
+});
